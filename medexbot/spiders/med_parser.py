4a24e6f885f71d687879cba17d7d3a1e9a8cf0bd
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-01-09 07:55:27 +0600

    Med links,pagination added

--- medexbot/spiders/med_parser.py
+++ medexbot/spiders/med_parser.py
@@ -7,3 +7,12 @@ class MedSpider(scrapy.Spider):
     name = "med"
     allowed_domains = ['medex.com.bd']
     start_urls = ['https://medex.com.bd/brands?page=1']
+
+    def parse(self, response):
+
+        for med_info in response.css('a.hoverable-block'):
+            med_page_links = med_info.css('a.hoverable-block ::attr("href") ')
+            print(med_page_links)
+            pagination_links = response.css('a.page-link[rel="next"]  ::attr("href") ')
+            # response.css('span[property="city"]::text').extract_first()
+            yield from response.follow_all(pagination_links, self.parse)
