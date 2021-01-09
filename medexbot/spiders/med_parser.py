54ac671243a5027d3c30867fcec9097211e54c31
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-01-09 07:59:11 +0600

    Med details init , name parsing done

--- medexbot/spiders/med_parser.py
+++ medexbot/spiders/med_parser.py
@@ -9,10 +9,15 @@ class MedSpider(scrapy.Spider):
     start_urls = ['https://medex.com.bd/brands?page=1']
 
     def parse(self, response):
-
         for med_info in response.css('a.hoverable-block'):
             med_page_links = med_info.css('a.hoverable-block ::attr("href") ')
-            print(med_page_links)
+            yield from response.follow_all(med_page_links, self.parse_med)
+
             pagination_links = response.css('a.page-link[rel="next"]  ::attr("href") ')
-            # response.css('span[property="city"]::text').extract_first()
             yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_med(self, response):
+        med_details = dict()
+        med_details['brand_name'] = response.xpath(
+            '/html/body/main/section/div[2]/div/div[1]/div/div[1]/h1/span[2]/text() ').get()
+        yield med_details
