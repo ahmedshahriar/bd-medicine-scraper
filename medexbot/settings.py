233293509822a44cd5c21cb8ee972280cef1f2bb
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-01-09 07:23:07 +0600

    User agent added in setting

--- medexbot/settings.py
+++ medexbot/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'medexbot.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'medexbot (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
