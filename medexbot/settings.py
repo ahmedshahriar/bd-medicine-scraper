65dd466a286d4d7417bde6f6e4cc7807c3fcbdd3
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-01-09 07:23:36 +0600

    telnet disabled

--- medexbot/settings.py
+++ medexbot/settings.py
@@ -34,7 +34,7 @@ ROBOTSTXT_OBEY = True
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
