cbcf4b65ab07ab66e71334ee2f5f3ee27711206b
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-05 02:21:55 +0600

    generic:parsed descriptions ongoing:more added

--- medexbot/spiders/generic_spider.py
+++ medexbot/spiders/generic_spider.py
@@ -40,6 +40,36 @@ class GenericSpider(scrapy.Spider):
         generic_details['pharmacology_description'] = response.xpath(
             '//div[@id="mode_of_action"]/following-sibling::node()[2]').get()
 
+        # ##Dosage
+        # dosage = extract_with_css('div#dosage h4 ::text')
+        dosage_description = response.xpath('//div[@id="dosage"]/following-sibling::node()[2]').get()
+
+        # ##Administration
+        # administration = extract_with_css('div#administration h4 ::text')
+        administration_description = response.xpath('//div[@id="administration"]/following-sibling::node()[2]').get()
+
+        # ##Interaction
+        # interaction = extract_with_css('div#interaction h4 ::text')
+        interaction_description = response.xpath('//div[@id="interaction"]/following-sibling::node()[2]').get()
+
+        # ##Contraindications
+        # contraindications = extract_with_css('div#contraindications h4 ::text')
+        contraindications_description = response.xpath(
+            '//div[@id="contraindications"]/following-sibling::node()[2]').get()
+
+        # ##Side Effects
+        # side_effects = extract_with_css('div#side_effects h4 ::text')
+        side_effects_description = response.xpath('//div[@id="side_effects"]/following-sibling::node()[2]').get()
+
+        # ##Pregnancy & Lactation
+        # pregnancy_and_lactation = extract_with_css('div#pregnancy_cat h4 ::text')
+        pregnancy_and_lactation_description = response.xpath(
+            '//div[@id="pregnancy_cat"]/following-sibling::node()[2]').get()
+
+        # ## Precautions
+        # precautions = extract_with_css('div#precautions h4 ::text')
+        precautions_description = response.xpath('//div[@id="precautions"]/following-sibling::node()[2]').get()
+
         item = GenericItem()
         for k, v in generic_details.items():
             item[k] = v
