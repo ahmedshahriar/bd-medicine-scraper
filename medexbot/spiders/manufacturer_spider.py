2ac403eb4444022765f2eb6e3a45b296e33fac1c
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-11 00:00:59 +0600

    manufacturer parser init

---
+++ medexbot/spiders/manufacturer_spider.py
@@ -0,0 +1,28 @@
+import re
+
+import scrapy
+
+
+class ManufacturerSpider(scrapy.Spider):
+    name = "manufacturer"
+    allowed_domains = ['medex.com.bd']
+    start_urls = ['https://medex.com.bd/companies?page=1']
+
+    def parse(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        # manufacturer_name = response.css('div.data-row-top a ::text').extract()
+        for company_info in response.css('div.data-row'):
+            manufacturer_link = company_info.css('div.data-row-top a ::attr(href)').get()
+            manufacturer_id = re.findall("companies/(\S*)/", manufacturer_link)[0]
+
+            yield {
+                "manufacturer_id": manufacturer_id,
+                "manufacturer_name": company_info.css('div.data-row-top a ::text').get(),
+                # "stat": company_info.xpath('//div[@class="data-row-top"]/following-sibling::node()[1]').get()
+                "stat": company_info.css('div.col-xs-12 ::text').extract()[-1].strip()
+            }
+
+        pagination_links = response.css('a.page-link[rel="next"]  ::attr("href") ')
+        yield from response.follow_all(pagination_links, self.parse)
