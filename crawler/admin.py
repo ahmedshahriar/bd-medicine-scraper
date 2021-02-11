497579214ba70f5948996dbf511762fe4981fc3e
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-11 13:31:50 +0600

    django:custom alphabetical filter added for all models admin

--- crawler/admin.py
+++ crawler/admin.py
@@ -5,11 +5,12 @@ from django.contrib import admin
 # Register your models here.
 from crawler.models import Medicine, Generic, Manufacturer
 
+
 # https://gist.github.com/ahmedshahriar/4240f0451261c4bb8364dd5341c7cf59
 # https://books.agiliq.com/projects/django-admin-cookbook/en/latest/filtering_calculated_fields.html
 
 class AlphabetFilter(admin.SimpleListFilter):
-    title = 'alphabet'
+    title = 'Name Alphabetically'
     parameter_name = 'alphabet'
 
     def lookups(self, request, model_admin):
@@ -17,19 +18,24 @@ class AlphabetFilter(admin.SimpleListFilter):
         return ((c.upper(), c.upper()) for c in abc)
 
     def queryset(self, request, queryset):
-        if self.value():
+        if self.value() and isinstance(queryset.model, Medicine):
+            return queryset.filter(brand_name__startswith=self.value())
+        if self.value() and isinstance(queryset.model, Generic):
+            return queryset.filter(generic_name__startswith=self.value())
+        if self.value() and isinstance(queryset.model, Manufacturer):
             return queryset.filter(manufacturer_name__startswith=self.value())
 
 
 @admin.register(Medicine)
 class MedicineAdmin(admin.ModelAdmin):
     list_display = ('brand_id', 'brand_name', 'dosage_form', 'generic_id')
-    list_filter = ('generic_id', 'created', 'dosage_form')
+    list_filter = ('generic_id', 'created', 'dosage_form', AlphabetFilter,)
     search_fields = ('brand_name', 'dosage_form')
     prepopulated_fields = {'slug': ('brand_name',)}
     # raw_id_fields = ('generic',)
     date_hierarchy = 'created'
     ordering = ('created',)
+
 
 @admin.register(Generic)
 class GenericAdmin(admin.ModelAdmin):
@@ -44,7 +50,7 @@ class GenericAdmin(admin.ModelAdmin):
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
     list_display = ('manufacturer_id', 'manufacturer_name', 'generics', 'brand_names')
-    list_filter = ('created', AlphabetFilter, )
+    list_filter = ('created', AlphabetFilter,)
     search_fields = ('manufacturer_name',)
     prepopulated_fields = {'slug': ('manufacturer_name',)}
     date_hierarchy = 'created'
