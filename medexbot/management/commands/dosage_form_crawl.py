c1164a706f781c6980391fcc09afee3908f61626
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-13 13:07:20 +0600

    dosage form brand_names count reformatted

---
+++ medexbot/management/commands/dosage_form_crawl.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from medexbot.spiders.dosage_form_spider import DosageFormSpider
+
+
+class Command(BaseCommand):
+    help = "Release the Dosage Form spider"
+
+    def handle(self, *args, **options):
+        process = CrawlerProcess(get_project_settings())
+        process.crawl(DosageFormSpider)
+        process.start()
--- medexbot/spiders/dosage_form_spider.py
+++ medexbot/spiders/dosage_form_spider.py
@@ -10,29 +10,31 @@ class DosageFormSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        for dosage_form_info in response.css('a.hoverable-block'):
+        for dosage_form_info in response.css('a.hoverable-block')[:30]:
             dosage_form_link = dosage_form_info.css('a.hoverable-block ::attr("href") ').get()
             dosage_form_id = re.findall("dosage-forms/(\S*)/", dosage_form_link)[0]
 
             dosage_form_name = dosage_form_info.css('div.data-row-top img ::attr("title") ').get()
-            stat = dosage_form_info.css('div.data-row-company ::text').extract()[-1].strip()
+            brand_names_counter = dosage_form_info.css('div.data-row-company ::text').re(r"(\d+)")
+            brand_names_count = 0 if len(brand_names_counter) == 0 else brand_names_counter
 
             yield from response.follow_all(dosage_form_info.css('a.hoverable-block ::attr("href") '),
                                            self.parse_dosage_form,
                                            meta={"dosage_form_id": dosage_form_id, "dosage_form_name": dosage_form_name,
-                                                 "stat": stat})
+                                                 "brand_names_count": brand_names_count})
 
     def parse_dosage_form(self, response):
         dosage_form_id = response.request.meta['dosage_form_id']
         dosage_form_name = response.request.meta['dosage_form_name']
-        stat = response.request.meta['stat']
+        brand_names_count = response.request.meta['brand_names_count']
 
         brand_name_links = response.css('a.hoverable-block  ::attr(href)').extract()
         brand_name_ids = [re.findall("brands/(\S*)/", brand_name_link)[0] for brand_name_link in brand_name_links]
 
-        yield {
+
+        print( {
             "dosage_form_id": dosage_form_id,
             "dosage_form_name": dosage_form_name,
-            "stat": stat,
+            "brand_names_count": brand_names_count,
             "brand_name_ids": brand_name_ids
-        }
+        })
