1360bf4d1fd519363ef689be4d2047df3dde6230
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-13 12:47:02 +0600

    dosage form related brand ids,counter parsed

--- medexbot/spiders/dosage_form_spider.py
+++ medexbot/spiders/dosage_form_spider.py
@@ -17,3 +17,22 @@ class DosageFormSpider(scrapy.Spider):
             dosage_form_name = dosage_form_info.css('div.data-row-top img ::attr("title") ').get()
             stat = dosage_form_info.css('div.data-row-company ::text').extract()[-1].strip()
 
+            yield from response.follow_all(dosage_form_info.css('a.hoverable-block ::attr("href") '),
+                                           self.parse_dosage_form,
+                                           meta={"dosage_form_id": dosage_form_id, "dosage_form_name": dosage_form_name,
+                                                 "stat": stat})
+
+    def parse_dosage_form(self, response):
+        dosage_form_id = response.request.meta['dosage_form_id']
+        dosage_form_name = response.request.meta['dosage_form_name']
+        stat = response.request.meta['stat']
+
+        brand_name_links = response.css('a.hoverable-block  ::attr(href)').extract()
+        brand_name_ids = [re.findall("brands/(\S*)/", brand_name_link)[0] for brand_name_link in brand_name_links]
+
+        yield {
+            "dosage_form_id": dosage_form_id,
+            "dosage_form_name": dosage_form_name,
+            "stat": stat,
+            "brand_name_ids": brand_name_ids
+        }
