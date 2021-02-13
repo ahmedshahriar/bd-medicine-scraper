14ee062a450a14a3d3832619ec437e38b47b5f3d
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-13 14:40:17 +0600

    indication: related generics added,parsing done

--- medexbot/spiders/indication_spider.py
+++ medexbot/spiders/indication_spider.py
@@ -14,8 +14,29 @@ class IndicationSpider(scrapy.Spider):
             indication_id = re.findall("indications/(\S*)/", indication_link)[0]
 
             indication_name = indication_info.css('div.data-row-top a ::text').get()
-            stat = indication_info.css('div.col-xs-12 ::text').extract()[-1].strip()
 
+            brand_names_counter = indication_info.css('div.col-xs-12 ::text').re(r"(\d+)")
+            generics_count = 0 if len(brand_names_counter) == 0 else brand_names_counter[0]
+
+            yield from response.follow_all(indication_info.css('div.data-row-top a ::attr(href)'),
+                                           self.parse_indication,
+                                           meta={"indication_id": indication_id, "indication_name": indication_name,
+                                                 "generics_count": generics_count})
 
         pagination_links = response.css('a.page-link[rel="next"]  ::attr("href") ')
         yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_indication(self, response):
+        indication_id = response.request.meta['indication_id']
+        indication_name = response.request.meta['indication_name']
+        generics_count = response.request.meta['generics_count']
+
+        generic_links = response.css('div.data-row-top a ::attr(href)').extract()
+        generic_ids = [re.findall("generics/(\S*)/", generic_link)[0] for generic_link in generic_links]
+
+        yield {
+            "indication_id": indication_id,
+            "indication_name": indication_name,
+            "generics_count": generics_count,
+            "generic_ids": generic_ids
+        }
