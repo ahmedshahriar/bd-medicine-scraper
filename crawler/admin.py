5c993c329e4e055a347738a8a2a881bd651811b0
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-13 14:09:59 +0600

    dosage form:admin updated

--- crawler/admin.py
+++ crawler/admin.py
@@ -3,7 +3,7 @@ import string
 from django.contrib import admin
 
 # Register your models here.
-from crawler.models import Medicine, Generic, Manufacturer
+from crawler.models import Medicine, Generic, Manufacturer, DosageForm
 
 
 # https://gist.github.com/ahmedshahriar/4240f0451261c4bb8364dd5341c7cf59
@@ -47,8 +47,8 @@ class MedicineAdmin(admin.ModelAdmin):
 
 @admin.register(Generic)
 class GenericAdmin(admin.ModelAdmin):
-    list_display = ('generic_id', 'generic_name', 'monograph_link', 'desc_counter')
-    list_filter = ('created', 'desc_counter')
+    list_display = ('generic_id', 'generic_name', 'monograph_link', 'descriptions_count')
+    list_filter = ('created', 'descriptions_count')
     search_fields = ('generic_name',)
     prepopulated_fields = {'slug': ('generic_name',)}
     date_hierarchy = 'created'
@@ -57,9 +57,19 @@ class GenericAdmin(admin.ModelAdmin):
 
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ('manufacturer_id', 'manufacturer_name', 'generics', 'brand_names')
+    list_display = ('manufacturer_id', 'manufacturer_name', 'generics_count', 'brand_names_count')
     list_filter = ('created', AlphabetFilter,)
     search_fields = ('manufacturer_name',)
     prepopulated_fields = {'slug': ('manufacturer_name',)}
+    date_hierarchy = 'created'
+    ordering = ('created',)
+
+
+@admin.register(DosageForm)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('dosage_form_id', 'dosage_form_name', 'brand_names_count')
+    list_filter = ('created', AlphabetFilter,)
+    search_fields = ('dosage_form_name',)
+    prepopulated_fields = {'slug': ('dosage_form_name',)}
     date_hierarchy = 'created'
     ordering = ('created',)
