d9e5f11911c76afcf3beef4e931f5148e1efa2ad
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-18 01:44:40 +0600

    api: indication,dosage form serializer added , url mapped

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('medicines/', views.MedicineListView.as_view(), name='medicine_list'),
     path('medicines/<pk>/', views.MedicineDetailView.as_view(), name='medicine_detail'),
     path('drug_classes/', views.DrugClassListView.as_view(), name='drug_class_list'),
+    path('drug_classes/<pk>/', views.DrugClassDetailView.as_view(), name='drug_class_detail'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,11 @@
 from rest_framework import generics
 
-from crawler.models import Medicine, DrugClass
-from .serializers import MedicineSerializer, DrugClassSerializer
+from crawler.models import Medicine, DrugClass, Generic, Indication, DosageForm
+from .serializers import MedicineSerializer, DrugClassSerializer, GenericSerializer, IndicationSerializer, \
+    DosageFormSerializer
 
 
 # Create your views here.
-
-
 class MedicineListView(generics.ListAPIView):
     queryset = Medicine.objects.all()
     serializer_class = MedicineSerializer
@@ -17,6 +16,41 @@ class MedicineDetailView(generics.RetrieveAPIView):
     serializer_class = MedicineSerializer
 
 
+class GenericListView(generics.ListAPIView):
+    queryset = Generic.objects.all()
+    serializer_class = GenericSerializer
+
+
+class GenericDetailView(generics.RetrieveAPIView):
+    queryset = Generic.objects.all()
+    serializer_class = GenericSerializer
+
+
 class DrugClassListView(generics.ListAPIView):
     queryset = DrugClass.objects.all()
     serializer_class = DrugClassSerializer
+
+
+class DrugClassDetailView(generics.RetrieveAPIView):
+    queryset = DrugClass.objects.all()
+    serializer_class = DrugClassSerializer
+
+
+class IndicationListView(generics.ListAPIView):
+    queryset = Indication.objects.all()
+    serializer_class = IndicationSerializer
+
+
+class IndicationDetailView(generics.RetrieveAPIView):
+    queryset = Indication.objects.all()
+    serializer_class = IndicationSerializer
+
+
+class DosageFormListView(generics.ListAPIView):
+    queryset = DosageForm.objects.all()
+    serializer_class = DosageFormSerializer
+
+
+class DosageFormDetailView(generics.RetrieveAPIView):
+    queryset = DosageForm.objects.all()
+    serializer_class = DosageFormSerializer
