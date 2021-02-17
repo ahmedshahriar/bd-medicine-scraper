0455e51a775f826a462855aefec0dbfd8fc2fdbe
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-18 01:46:51 +0600

    api: generic, dosage form url adeed

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('medicines/<pk>/', views.MedicineDetailView.as_view(), name='medicine_detail'),
     path('drug_classes/', views.DrugClassListView.as_view(), name='drug_class_list'),
     path('drug_classes/<pk>/', views.DrugClassDetailView.as_view(), name='drug_class_detail'),
+    path('generics/', views.GenericListView.as_view(), name='generic_list'),
+    path('generics/<pk>/', views.GenericDetailView.as_view(), name='generic_detail'),
+    path('dosage_forms/', views.DosageFormListView.as_view(), name='dosage_forms_list'),
+    path('dosage_forms/<pk>/', views.DosageFormDetailView.as_view(), name='dosage_forms_detail'),
 
 ]
