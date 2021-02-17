afd505333ec45b3b4d9ddc89e4e19313cd89e2dd
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-18 01:49:13 +0600

    api: indication, manufacturer  url adeed

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path('drug_classes/<pk>/', views.DrugClassDetailView.as_view(), name='drug_class_detail'),
     path('generics/', views.GenericListView.as_view(), name='generic_list'),
     path('generics/<pk>/', views.GenericDetailView.as_view(), name='generic_detail'),
-    path('dosage_forms/', views.DosageFormListView.as_view(), name='dosage_forms_list'),
-    path('dosage_forms/<pk>/', views.DosageFormDetailView.as_view(), name='dosage_forms_detail'),
+    path('dosage_forms/', views.DosageFormListView.as_view(), name='dosage_form_list'),
+    path('dosage_forms/<pk>/', views.DosageFormDetailView.as_view(), name='dosage_form_detail'),
+    path('manufacturers/', views.ManufacturerListView.as_view(), name='manufacturer_list'),
+    path('manufacturers/<pk>/', views.ManufacturerDetailView.as_view(), name='manufacturer_detail'),
+    path('indications/', views.IndicationListView.as_view(), name='indications_list'),
+    path('indications/<pk>/', views.IndicationDetailView.as_view(), name='indications_detail'),
 
 ]
