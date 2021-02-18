bd773d3a4a000ce77064bc77f8aaeac8ee2fc56a
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-18 12:41:43 +0600

    manufacturer parser slug updated, itemified

--- medexbot/spiders/manufacturer_spider.py
+++ medexbot/spiders/manufacturer_spider.py
@@ -1,6 +1,7 @@
 import re
 
 import scrapy
+from django.utils.text import slugify
 
 from medexbot.items import ManufacturerItem
 
@@ -17,7 +18,7 @@ class ManufacturerSpider(scrapy.Spider):
         # "stat": [int(s) for s in (company_info.css('div.col-xs-12 ::text').extract()[-1].strip()).split()
         # if s.isdigit()]
         for company_info in response.css('div.data-row'):
-            item = dict()
+            item = ManufacturerItem()
             manufacturer_link = company_info.css('div.data-row-top a ::attr(href)').get()
             generic_counter, brand_name_counter = (int(s) for s in (
                 company_info.css('div.col-xs-12 ::text').extract()[-1].strip()).split() if s.isdigit())
@@ -26,7 +27,8 @@ class ManufacturerSpider(scrapy.Spider):
             item["manufacturer_name"] = company_info.css('div.data-row-top a ::text').get()
             item["generics_count"] = generic_counter
             item["brand_names_count"] = brand_name_counter
-
+            item['slug'] = slugify(item['manufacturer_name'] + '-' + item['manufacturer_id'],
+                                   allow_unicode=True)
             yield item
 
         pagination_links = response.css('a.page-link[rel="next"]  ::attr("href") ')
