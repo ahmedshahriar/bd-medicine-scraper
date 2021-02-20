80f5725670c74bf7e9c49508b8d206f900761981
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-20 11:56:59 +0600

    django:token fetch url updated

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth', obtain_auth_token, name='get_token'),
     path('api/', include('api.urls', namespace='api')),
 ]
